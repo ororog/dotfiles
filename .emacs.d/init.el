7aec86dac0f9079b11ed99776a96324800984bb3
ororog <ororog@gmail.com>
2017-12-18 21:30:51 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,12 +24,16 @@
    rubocop
    flycheck
    scss-mode
+   js2-mode
+   magit
+   jedi
    ))
 
 (unless package-archive-contents
   (package-refresh-contents))
 
 ;; Global settings
+(electric-indent-mode -1)
 (tool-bar-mode -1)
 (global-unset-key "\C-z")
 (setq-default show-trailing-whitespace t)
@@ -51,6 +55,7 @@
 (highlight-symbol-mode t)
 (setq-default highlight-symbol-idle-delay 0.3)
 (prefer-coding-system 'utf-8)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Faces custmization
 (defun describe-face-at-point ()
@@ -87,15 +92,42 @@
 (projectile-global-mode)
 (helm-projectile-on)
 (custom-set-variables
- '(helm-truncate-lines t)
- '(helm-delete-minibuffer-contents-from-point t)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(css-indent-offset 2)
+ '(enh-ruby-deep-indent-paren nil)
+ '(enh-ruby-hanging-indent-level 4)
+ '(enh-ruby-hanging-paren-indent-level 4)
+ '(flycheck-checker (quote ruby-rubocop) t)
  '(helm-candidate-number-limit 100)
+ '(helm-delete-minibuffer-contents-from-point t)
  '(helm-for-files-preferred-list
-   '(helm-source-recentf
-     helm-source-projectile-files-list
-     helm-source-buffers-list
-     helm-source-files-in-current-dir))
- '(projectile-completion-system 'helm))
+   (quote
+    (helm-source-recentf helm-source-projectile-files-list helm-source-buffers-list helm-source-files-in-current-dir)))
+ '(helm-truncate-lines t t)
+ '(js-indent-level 2)
+ '(js2-basic-offset 2)
+ '(package-selected-packages
+   (quote
+    (company-jedi jedi web-mode scss-mode ruby-block rubocop recentf-ext magit js2-mode highlight-symbol helm-projectile flycheck exec-path-from-shell enh-ruby-mode)))
+ '(projectile-completion-system (quote helm))
+ '(projectile-globally-ignored-file-suffixes (quote ("png" "jpg")))
+ '(projectile-sort-order (quote recently-active))
+ '(recentf-auto-cleanup 10)
+ '(recentf-auto-save-timer nil)
+ '(recentf-exclude (quote (".recentf")))
+ '(recentf-max-saved-items 1000)
+ '(ruby-insert-encoding-magic-comment nil)
+ '(scss-compile-at-save nil)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-enable-auto-closing t)
+ '(web-mode-enable-auto-indentation nil)
+ '(web-mode-enable-current-element-highlight t)
+ '(web-mode-markup-indent-offset 2))
+(add-to-list 'projectile-globally-ignored-directories "tmp")
+(add-to-list 'projectile-globally-ignored-directories "log")
 (define-key global-map (kbd "M-x") 'helm-M-x)
 (define-key global-map (kbd "C-x C-f") 'helm-for-files)
 (define-key global-map (kbd "C-x f") 'helm-find-files)
@@ -107,6 +139,7 @@
 (autoload 'flycheck "flycheck" "Realtime ruby syntax checker.")
 (autoload 'rubocop "rubocop" "Syntax check definition.")
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (defun ruby-mode-hooks ()
   "Hooks for ruby."
@@ -116,18 +149,18 @@
    '(enh-ruby-deep-indent-paren nil)
    '(enh-ruby-hanging-indent-level 4)
    '(enh-ruby-hanging-paren-indent-level 4))
+  (flycheck-mode 1)
   (font-lock-add-keywords
    nil '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))
-  (flycheck-mode t))
+  (font-lock-add-keywords
+   nil '((" \\{2,\\}" 1 font-lock-warning-face t)) t))
 (add-hook 'enh-ruby-mode-hook 'ruby-mode-hooks)
 (defun remove-enh-magic-comment ()
   (remove-hook 'before-save-hook 'enh-ruby-mode-set-encoding t))
 (add-hook 'enh-ruby-mode-hook 'remove-enh-magic-comment)
 
 ;; git setting
-(custom-set-variables
- '(projectile-globally-ignored-file-suffixes '("png" "jpg"))
- '(projectile-sort-order 'recently-active))
+
 
 ;; web-mode
 (defun web-mode-hooks ()
@@ -138,7 +171,8 @@
   (custom-set-variables
    '(web-mode-enable-current-element-highlight t)
    '(web-mode-enable-auto-indentation nil)
-   '(web-mode-code-indent-offset 8)'
+   '(web-mode-code-indent-offset 2)
+   '(web-mode-markup-indent-offset 2)
    '(web-mode-enable-auto-closing t)))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
@@ -148,10 +182,53 @@
 (autoload 'scss-mode "scss-mode")
 (defun scss-mode-hooks ()
   "Hooks for scss mode."
-  (setq-default css-indent-offset 2)
   (custom-set-variables
    '(css-indent-offset 2)
-   '(scss-compile-at-save nil)))
-
+   '(scss-compile-at-save nil))
+  (rubocop-mode nil)
+  (flycheck-mode t))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
-(add-hook 'scss-mode 'scss-mode-hooks)
+(add-hook 'scss-mode-hook 'scss-mode-hooks)
+
+;; js2-mode
+(defun js2-mode-hooks ()
+  "Hooks for js2 mode."
+  (font-lock-add-keywords
+   nil '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))
+  (custom-set-variables
+   '(js2-basic-offset 2)))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook 'js2-mode-hooks)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; ispell
+(setq-default ispell-program-name "aspell")
+(eval-after-load "ispell"
+ '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+
+;; magit
+(require 'magit)
+(setq ediff-split-window-function 'split-window-horizontally)
+(defun ediff-mode-hooks ()
+  (set-frame-parameter initial-frame-alist 'alpha '(100 100)))
+(defun ediff-quit-hooks ()
+  (set-frame-parameter initial-frame-alist 'alpha '(85 65)))
+(add-hook 'ediff-mode-hook 'ediff-mode-hooks)
+(add-hook 'magit-ediff-quit-hook 'ediff-quit-hooks)
+
+;; python
+(defun python-mode-hooks()
+  (setq-default indent-tabs-mode nil)
+  (setq-default python-indent 2)
+  (setq-default indent-level 2)
+  (setq-default tab-width 2))
+(add-hook 'python-mode-hook 'python-mode-hooks)
+
+(require 'jedi)
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
--- .zshrc
+++ .zshrc
@@ -1,3 +1,6 @@
+# general
+export LC_ALL='ja_JP.UTF-8'
+
 # ruby setting
 eval "$(rbenv init -)"
 alias s='source ~/.zshrc'
@@ -20,7 +23,21 @@ zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'c
 
 # history
 export HISTFILE=${HOME}/.zsh_history
-export HISTSIZE=1000
+export HISTSIZE=10000
 export SAVEHIST=100000
 setopt hist_ignore_dups
 setopt EXTENDED_HISTORY
+setopt share_history
+
+# tmux fix
+alias subl='reattach-to-user-namespace subl'
+alias stree='reattach-to-user-namespace stree'
+alias open='reattach-to-user-namespace open'
+
+# pyenv
+export PYENV_ROOT="${HOME}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:$PATH"
+eval "$(pyenv init -)"
+
+# postgresql@9.5
+export PATH="/usr/local/opt/postgresql@9.5/bin:$PATH"
